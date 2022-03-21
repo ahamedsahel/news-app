79eb6804781485c1667adc49c47aa7315ceff9d0
ahammed sahal <ahammed.sahl@igrant.io>
2022-03-21 18:15:26 +0530

    updated

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,56 +1,50 @@
 import React, { useState, useEffect } from "react";
 import "./home.css";
 import axios from "axios";
-import Select from 'react-select'
+import Select from "react-select";
 
 function App() {
   const [news, setNews] = useState([]);
 
-console.log(" Sahal : ",news);
- console.log(news);
+  console.log(news);
   useEffect(() => {
     const getArticles = async () => {
-            const response = await axios.get(`
-            https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=2b6fe601e65246cc88b85fdbb3792005`)
-            setNews(response.data.articles)
-             }
-   
-            
-        
+      const response = await axios.get(`
+           
+https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd44be8da0ec4f148fbf5ad201725ed1`);
+      setNews(response.data.articles);
+    };
 
-        getArticles()
-       
+    getArticles();
   }, []);
 
   const options = [
     { value: "Afghanistan", label: "Afghanistan" },
-    { value: 'Algeria', label: "Algeria" },
+    { value: "Algeria", label: "Algeria" },
     { value: "Cambodia", label: "Cambodia" },
-    {value: "India" , label : "India"}
-  ]
-  
+    { value: "India", label: "India" },
+  ];
+
   return (
     <div>
       <div className="drop-down">
-      
-      <Select className="menu" placeholder="Select..." options={options} />
+        <Select className="menu" placeholder="Select..." options={options} />
       </div>
       <div className="container mt-5">
         <div className="row">
           {news.map((value, index) => {
             return (
               <div key={index} className="col-4">
-              
                 <div
                   className="card mt-4"
-                  style={{ width: "23rem", lineHeight: "normal", }}
+                  style={{ width: "23rem", lineHeight: "normal" }}
                 >
                   <img
                     src={value.urlToImage}
                     className="card-img-top"
                     alt="..."
                   />
-              
+
                   <p className="name">{value.source.name}</p>
                   <div className="card-body">
                     <h5 className="card-title">{value.title}</h5>
